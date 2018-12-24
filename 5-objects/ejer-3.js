0fa95fa9f773c89dcc2e7f8b7135ee30019dc2e6
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2018-12-24 18:07:25 +0100

    The prime numbers was an excelent idea

--- 5-objects/ejer-3.js
+++ 5-objects/ejer-3.js
@@ -1,1 +1,7 @@
-// Code a program to get a list with the 20 first pair numbers
+// Code a program to get a list with the 20 first prime numbers
+
+let primeNumbers = [];
+let index = 0;
+
+
+console.log(primeNumbers);
