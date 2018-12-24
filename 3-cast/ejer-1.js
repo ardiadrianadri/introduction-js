1ae9f962d35c2ce61bd2424971a0cb1ae5503d11
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2018-12-24 08:06:10 +0100

    Add exercice about the out cast

---
+++ 3-cast/ejer-1.js
@@ -0,0 +1,12 @@
+// Try to guess what is the output of these lines of code
+
+let aString='a';
+let noValue = undefined;
+console.log(3 + true);
+console.log('3' + 1);
+console.log('34' < '5');
+
+aString+=++aString;
+console.log(aString);
+
+console.log('Hello ' + noValue);
