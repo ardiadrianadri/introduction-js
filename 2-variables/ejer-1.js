fc59ae31309522823438de0a7a8443ab8deb429a
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2018-12-24 07:53:35 +0100

    Exercice to understand the increment operators

---
+++ 2-variables/ejer-1.js
@@ -0,0 +1,12 @@
+// Try to guess which is the conosle output of these code
+let aNumber = 5;
+let aString = 'a';
+
+console.log(++aNumber);
+console.log(aNumber--);
+
+aNumber+=6;
+console.log(aNumber);
+
+aString+='lfombra';
+console.log(aString);
