0fcb7f3958191e123eecfd8ff031617650d0e957
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2018-12-24 18:43:27 +0100

    Add a new exercise for objects

---
+++ 5-objects/ejer-5.js
@@ -0,0 +1,6 @@
+// Can you tell me what is the output of this code?
+
+console.log(null == undefined);
+console.log(null === undefined);
+console.log(NaN == undefined || NaN == null);
+console.log(NaN == NaN);
